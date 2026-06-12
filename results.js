6d9ed4083b07e2f438419712262aed5c9c25804d
Antonio A. García <72597896+antonioabelgc@users.noreply.github.com>
2026-06-12 07:57:23 +0200

    Update results.js

--- results.js
+++ results.js
@@ -17,6 +17,7 @@ const RESULTS = {
   "groupMatches": {
     "A": {
       "Mexico__South Africa": { "home": 2, "away": 0 },
+      "South Korea__Czech Republic": { "home": 2, "away": 1 }
     },
     "B": {},
     "C": {},
